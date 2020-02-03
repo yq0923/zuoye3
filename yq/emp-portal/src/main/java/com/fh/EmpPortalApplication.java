e697ea41da096ccf50c7c4703c4b876b9889c706
yq <2867661200@qq.com>
2020-02-03 18:14:03 +0800

    提交2

---
+++ yq/emp-portal/src/main/java/com/fh/EmpPortalApplication.java
@@ -0,0 +1,13 @@
+package com.fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpPortalApplication.class, args);
+    }
+
+}
